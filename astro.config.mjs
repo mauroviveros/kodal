6b81cb965e953fc254dcf882e58e55bc84e726be
Mauro Daniel viveros <maurod.viveros@gmail.com>
2025-08-28 16:07:06 -0300

    chore: :package: add @astrojs/vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel';
 import tailwindcss from '@tailwindcss/vite';
 import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: vercel(),
   integrations: [icon()],
   vite: {
     plugins: [tailwindcss()]
