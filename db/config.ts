592088780c29e7217a276d55ab9e4ce0885c6c79
Mauro Daniel viveros <maurod.viveros@gmail.com>
2025-09-07 04:17:55 -0300

    feat: :alien: implement PUT and POST api

--- db/config.ts
+++ db/config.ts
@@ -3,12 +3,12 @@ import { column, defineDb, defineTable } from 'astro:db';
 const Pet = defineTable({
   columns: {
     id: column.text({ primaryKey: true }),
-    name: column.text(),
-    species: column.text({ enum: ['dog', 'cat'] }),
-    breed: column.text(),
-    birthdate: column.date(),
-    weight: column.number(),
-    gender: column.text({ enum: ['male', 'female', 'unknown'] }),
+    name: column.text({ optional: true }),
+    species: column.text({ enum: ['dog', 'cat', 'other'], optional: true }),
+    breed: column.text({ optional: true }),
+    birthdate: column.date({ optional: true }),
+    weight: column.number({ optional: true }),
+    gender: column.text({ enum: ['male', 'female', 'unknown'], optional: true }),
     message: column.text({ optional: true }),
   }
 });
---
+++ src/pages/api/create.ts
@@ -0,0 +1,22 @@
+import type { APIRoute } from "astro";
+import { v4 as uuid } from 'uuid';
+import sha256 from 'crypto-js/hmac-sha256';
+import { db, Pet } from "astro:db";
+
+export const POST: APIRoute = async () => {
+  try {
+    const id = sha256(uuid() + new Date().toISOString(), 'miclavesecreta').toString();
+    await db.insert(Pet).values({ id });
+
+    return new Response(
+      JSON.stringify({ id, message: 'OK' }),
+      { status: 200 }
+    );
+  } catch (error) {
+    console.log(error);
+    return new Response(
+      JSON.stringify({ ...error as Error }),
+      { status: 500 }
+    );
+  }
+}
