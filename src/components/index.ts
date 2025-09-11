e36f2486e773917dd8089dad0f67443cd0a6b488
Mauro Daniel viveros <maurod.viveros@gmail.com>
2025-09-11 14:54:53 -0300

    feat: :sparkles: add mplement Hero section, and update layout structure

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,6 @@
 export { default as AvatarForm } from "./forms/AvatarForm.astro";
 export { default as BasicForm } from "./forms/BasicForm.astro";
-export { default as Avatar } from "./Avatar.astro";
 export { default as Brand } from "./Brand.astro";
-export { default as Card } from "./Card.astro";
-export { default as Button } from "./Button.astro";
-export { default as SEO } from "./SEO.astro";
+
+export { default as Header } from "./Header.astro";
+export { default as Footer } from "./Footer.astro";
---
+++ src/components/ui/index.ts
@@ -0,0 +1,3 @@
+export { default as Avatar } from "./Avatar.astro";
+export { default as Card } from "./Card.astro";
+export { default as Button } from "./Button.astro";
