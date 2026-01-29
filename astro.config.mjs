b51306ed2160855668460e5091455dcc1f1b5476
Mauro Daniel viveros <maurod.viveros@gmail.com>
2026-01-29 06:56:58 -0300

    feat: :card_file_box: add supabase integration and new schema

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,11 @@ import vercel from '@astrojs/vercel';
 import tailwindcss from '@tailwindcss/vite';
 import icon from 'astro-icon';
 
-import db from '@astrojs/db';
-
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
-  integrations: [icon(), db()],
+  integrations: [icon()],
   vite: {
     plugins: [tailwindcss()],
   },
---
+++ src/lib/supabase.ts
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+import type { Database } from "./database";
+
+export const supabase = createClient<Database>(
+  import.meta.env.SUPABASE_URL,
+  import.meta.env.SUPABASE_KEY,
+);
