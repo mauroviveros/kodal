8fa9a09a7a0c06c54966d5f6a5ad756a04b8ade5
Mauro Daniel viveros <maurod.viveros@gmail.com>
2026-01-30 20:22:41 -0300

    feat: :goal_net: improved error handling and response structure

--- src/pages/api/slot/[id]/medal.ts
+++ src/pages/api/slot/[id]/medal.ts
@@ -2,7 +2,7 @@ import { supabase } from "@lib/supabase";
 import type { Medal, Slot } from "@types";
 import type { APIRoute } from "astro";
 
-export const POST: APIRoute = async ({ params, request }) => {
+export const POST: APIRoute = async ({ params, request, redirect }) => {
   let slot_db: Slot = null;
   let medal_db: Medal = null;
 
@@ -10,43 +10,53 @@ export const POST: APIRoute = async ({ params, request }) => {
   const entries = formData.entries().map(([key, val]) => [key, val || undefined]);
   const data = Object.fromEntries(entries);
 
-  if (params.id) {
-    const { data } = await supabase.from("medals_slot").select("*").eq("id", params.id).single();
-    slot_db = data;
+  try {
+    if (params.id) {
+      const { data, error } = await supabase.from("medals_slot").select("*").eq("id", params.id).maybeSingle();
+      if (error) throw error;
+      slot_db = data;
+    }
+
+    if (!slot_db) throw new Error("Slot undefined");
+
+    const { data: medal_upserted, error } = await supabase.from("medals").upsert({ id: slot_db.medal_id || undefined, ...data}).select().single();
+    if (error) throw error;
+    medal_db = medal_upserted;
+
+    if (!slot_db.medal_id && medal_db) {
+      const { data: slot_updated, error } = await supabase.from("medals_slot").update({ medal_id: medal_db.id, used_at: new Date().toISOString() }).eq("id", slot_db.id).select().single();
+      if (error) throw error;
+      if( slot_updated ) slot_db = slot_updated;
+    }
+
+    return redirect(`/medal/${slot_db.id}`);
+  } catch(error){
+    if(error instanceof Error) error = { message: error.message };
+    return new Response(JSON.stringify({ slot: slot_db, medal: medal_db, error }), { status: 500, headers: { "Content-Type": "application/json" } } );
   }
-
-  if (!slot_db) {
-    return new Response(JSON.stringify({ error: "Slot no encontrado" }), { status: 404 });
-  }
-
-  const { data: medal_upserted } = await supabase.from("medals").upsert({ id: slot_db.medal_id || undefined, ...data}).select().single();
-  medal_db = medal_upserted;
-
-  if (!slot_db.medal_id && medal_db) {
-    const { data: slot_updated } = await supabase.from("medals_slot").update({ medal_id: medal_db.id, used_at: new Date().toISOString() }).eq("id", slot_db.id).select().single()
-    slot_db = slot_updated;
-  }
-
-  return new Response(JSON.stringify({ slot: slot_db, medal: medal_db }), { status: 200 });
 }
 
 export const GET: APIRoute = async ({ params }) => {
   let slot: Slot = null;
   let medal: Medal = null;
 
-  try {
-    if ( params.id ){
-      const { data } = await supabase.from('medals_slot').select('*').eq('id', params.id || '').single();
-      slot = data;
-    }
+  if (!params.id) return new Response(JSON.stringify({ slot, medal }), { status: 200 });
+
+  try{
+    const { data, error } = await supabase.from('medals_slot').select('*').eq('id', params.id).maybeSingle();
+    if (error) throw error;
+    slot = data;
 
     if ( slot?.medal_id ) {
-      const { data } = await supabase.from('medals').select('*').eq('id', slot.medal_id).single();
+      const { data, error } = await supabase.from('medals').select('*').eq('id', slot.medal_id).maybeSingle();
+      if (error) throw error;
       medal = data;
     }
 
-    return new Response(JSON.stringify({slot, medal}), { status: 200 });
-  } catch (error) {
-    return new Response(JSON.stringify({ error }), { status: 500 });
+    return new Response(JSON.stringify({slot, medal}), { status: 200, headers: { "Content-Type": "application/json" } });
+  } catch(error){
+    return new Response(JSON.stringify({slot, medal, error}), { status: 500, headers: { "Content-Type": "application/json" } });
   }
+
+
 }
