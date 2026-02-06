954157e0909e31985b92a322100fef56dbb1048a
Mauro Daniel viveros <maurod.viveros@gmail.com>
2026-02-06 16:06:15 -0300

    refactor: :recycle: remove generateToken action and clean up related code in index and pets files feat: :sparkles: add email sent confirmation page for identity verification process feat: :sparkles: enhance medal index page with redirect after sending token email

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -3,14 +3,12 @@ import insertPetForm from './insertPetForm';
 import updatePetForm from './updatePetForm';
 import createBulkMedals from './createBulkMedals';
 import getMedal from './getMedal';
-import { generateToken } from './pets';
 import getMedalsPaginated from './getMedalsPaginated';
 import sendTokenEmail from './sendTokenEmail';
 
 export const server = {
   getMedal,
   getMedalsPaginated,
-  generateToken,
   insertPetForm,
   updatePetForm,
   getToken,
