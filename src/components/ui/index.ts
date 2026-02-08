d54e2261426d05da97d4768b7e9bfd452270949e
Mauro Daniel viveros <maurod.viveros@gmail.com>
2026-02-08 16:58:37 -0300

    refactor: :recycle: consolidate Card imports and remove deprecated Card component files for improved structure

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,7 +1,9 @@
 export { default as Avatar } from './Avatar.astro';
 export { default as Button } from './Button.astro';
+export { default as Card } from './Card.astro';
 export { default as Dialog } from './Dialog.astro';
 export { default as Fieldset } from './Fieldset.astro';
 export { default as Input } from './Input.astro';
 export { default as Link } from './Link.astro';
 export { default as Select } from './Select.astro';
+export { default as Table } from './Table.astro';
