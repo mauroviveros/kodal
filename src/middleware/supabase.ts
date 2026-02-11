27085ed3c1c57fa53bf862a54d6c1fb8cfecaebb
Mauro Daniel viveros <maurod.viveros@gmail.com>
2026-02-11 11:56:10 -0300

    feat: ✨ add prerendering support

--- src/middleware/supabase.ts
+++ src/middleware/supabase.ts
@@ -2,7 +2,9 @@ import { createClient } from '@supabase';
 import type { MiddlewareHandler } from 'astro';
 
 // Este middleware se encarga de crear un cliente de Supabase para cada solicitud,
-export const middleware: MiddlewareHandler = async ({ request, cookies, locals }, next) => {
+export const middleware: MiddlewareHandler = async ({ request, cookies, locals, isPrerendered }, next) => {
+  if (isPrerendered) return next(); // Si la página se está prerenderizando, saltamos este middleware para evitar errores
+
   const supabase = createClient(request, cookies);
   const {
     data: { session },
