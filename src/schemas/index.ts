3981f9ec5f7b1aee39601922271c916054828353
Mauro Daniel viveros <maurod.viveros@gmail.com>
2026-02-11 15:42:07 -0300

    feat: :children_crossing: enhance view transitions

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -3,18 +3,18 @@ import { z } from 'astro/zod';
 
 const medalSchema = z.object({
   medal_id: z.string().uuid(),
-  medal_full_name: z.string().min(1).max(50),
+  medal_full_name: z.string().trim().min(1).max(50),
   medal_email: z.string().email().max(100),
-  medal_phone: z.string().min(7).max(20).optional(),
+  medal_phone: z.string().trim().min(7).max(20).optional(),
   medal_relation_type: z.enum(Constants.public.Enums.MEDAL_RELATION),
 });
 
 const petSchema = z.object({
   id: z.string().uuid(),
-  name: z.string().min(1).max(50),
+  name: z.string().trim().min(1).max(50),
   species: z.enum(Constants.public.Enums.PET_SPECIES),
   gender: z.enum(Constants.public.Enums.PET_GENDER),
-  breed: z.string().min(1).max(50).optional(),
+  breed: z.string().trim().min(1).max(50).optional(),
   birth_date: z.string().optional(),
   avatar_file: z.instanceof(File).optional(),
 });
