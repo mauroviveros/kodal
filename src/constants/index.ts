c6f322dfbf85b00d77697b30e4268e4c44a0b489
Mauro Daniel viveros <maurod.viveros@gmail.com>
2026-02-11 00:20:00 -0300

    feat: ✨ add support page with contact methods and FAQs

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,45 +1,6 @@
-import type { Enums } from "@types";
+export * from './medals';
+export * from './landing';
 
 export const RESEND_TEMPLATES = {
   IDENTITY_VERIFICATION: 'kodal-token-1',
 }
-
-export const MEDAL_RELATIONS: Record<Enums<'MEDAL_RELATION'>, string> = {
-  OWNER: 'Dueño/a',
-  FATHER: 'Padre',
-  MOTHER: 'Madre',
-  GUARDIAN: 'Tutor/a',
-  VETERINARIAN: 'Veterinario/a',
-  EMERGENCY: 'Contacto de emergencia',
-};
-
-export const MEDAL_STATUS: Record<
-  Enums<'MEDAL_STATUS'>,
-  { text: string; icon: string; class: string }
-> = {
-  CREATED: {
-    text: 'Nuevo',
-    icon: 'lucide:sparkles',
-    class: 'text-blue-600 bg-blue-100 border-blue-300!',
-  },
-  MANUFACTURED: {
-    text: 'Fabricado',
-    icon: 'lucide:factory',
-    class: 'text-yellow-600 bg-yellow-100 border-yellow-300',
-  },
-  ACTIVE: {
-    text: 'En uso',
-    icon: 'lucide:user-check',
-    class: 'text-emerald-600 bg-emerald-100 border-emerald-300!',
-  },
-  LOST: {
-    text: 'Perdido',
-    icon: 'lucide:alert-triangle',
-    class: 'text-red-600 bg-red-100 border-red-300',
-  },
-  DISABLED: {
-    text: 'Desactivado',
-    icon: 'lucide:slash',
-    class: 'text-gray-600 bg-gray-100 border-gray-300',
-  },
-};
---
+++ src/constants/landing.ts
@@ -0,0 +1,30 @@
+
+export const SUPPORT_FREQUENTLY_ASKED_QUESTIONS: { question: string, answer: string }[] = [
+  {
+    question: '¿Cómo actualizo la información de mi mascota?',
+    answer: 'Puedes actualizar toda la información desde la página de edición. Solo haz clic en "Editar" en el perfil de tu mascota (en la parte superior derecha) y modifica los datos que necesites.',
+  },
+  {
+    question: '¿Qué hago si pierdo la medalla?',
+    answer: 'Contacta inmediatamente con nuestro soporte para desactivar la medalla perdida y solicitar una nueva. Mientras tanto, puedes compartir el enlace del perfil digital directamente.'
+  },
+  {
+    question: '¿La medalla funciona sin internet?',
+    answer: 'La medalla necesita conexión a internet para mostrar la información actualizada.'
+  }
+];
+
+export const SUPPORT_CONTACT_METHODS: { method: string, contact: string, description: string, icon: string }[] = [
+  {
+    method: 'Teléfono de soporte',
+    contact: '+54 9 11 1234-5678',
+    description: 'Lunes a Viernes, 08:00 - 18:00',
+    icon: 'lucide:phone',
+  },
+  {
+    method: 'Correo de soporte',
+    contact: 'maurod.viveros@gmail.com',
+    description: 'Respuesta en 24 horas',
+    icon: 'lucide:mail',
+  }
+];
