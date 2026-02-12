429a704a3498e0f3f1d09c80631897779c7da5e7
Mauro Daniel viveros <maurod.viveros@gmail.com>
2026-02-12 00:00:49 -0300

    chore: :mute: remove console.log

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -13,7 +13,6 @@ const isAPI = (pathname: string) => minimatch(pathname, '/api/**');
 
 // Este middleware se encarga de verificar si el usuario tiene una sesión válida para acceder a rutas protegidas
 export const middleware: MiddlewareHandler = async ({ locals: { supabase, session }, url, redirect }, next) => {
-  console.time('Auth Middleware');
   if (isProtected(url.pathname)) {
     const { data } = await supabase.auth.getSession();
     session = data.session;
@@ -22,6 +21,5 @@ export const middleware: MiddlewareHandler = async ({ locals: { supabase, sessio
       return redirect(`/admin/signin?redirectTo=${encodeURIComponent(url.pathname)}`);
     }
   }
-  console.timeEnd('Auth Middleware');
   return next();
 };
--- src/middleware/supabase.ts
+++ src/middleware/supabase.ts
@@ -3,9 +3,7 @@ import type { MiddlewareHandler } from 'astro';
 
 // Este middleware se encarga de crear un cliente de Supabase para cada solicitud,
 export const middleware: MiddlewareHandler = async ({ request, cookies, locals, isPrerendered }, next) => {
-  console.time('Supabase Middleware');
   if (isPrerendered) return next(); // Si la página se está prerenderizando, saltamos este middleware para evitar errores
   locals.supabase = createClient(request, cookies);
-  console.timeEnd('Supabase Middleware');
   return next();
 };
