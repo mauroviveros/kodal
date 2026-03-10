e2e4a6734247217f0c0b793104602f10ba1dc857
Mauro Daniel viveros <maurod.viveros@gmail.com>
2026-03-10 19:10:11 -0300

    fix: :adhesive_bandage: update image domain configuration and wrap button in main tag for structure

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: [process.env.NEXT_PUBLIC_SUPABASE_URL || ""],
+    domains: [process.env.NEXT_PUBLIC_SUPABASE_URL?.replace('https://', '') || ""],
   }
   /* config options here */
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <>
-    <Button variant="outline" size="lg">Click Me</Button>
-    </>
+    <main className="">
+      <Button variant="outline" size="lg">Click Me</Button>
+    </main>
   );
 }
