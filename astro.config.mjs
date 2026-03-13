806eab453de78a641cd3f5bd9f09b630d49f2f52
Mauro Daniel viveros <maurod.viveros@gmail.com>
2026-03-13 18:52:24 -0300

    feat: :construction: integrate React islands for forms

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,15 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import vercel from '@astrojs/vercel';
 import icon from 'astro-icon';
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.kodal.pet',
   output: 'server',
   vite: {
     plugins: [tailwindcss()]
   },
-
-  integrations: [icon()],
+  integrations: [icon(), react()],
   adapter: vercel()
 });
---
+++ src/components/forms/edit/Form.tsx
@@ -0,0 +1,5 @@
+export const Form = () => {
+  return (
+    <h1>React Component</h1>
+  )
+}
