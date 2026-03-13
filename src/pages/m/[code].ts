15c3c09d8ebbd4fbfa1ccb0eac72f347edafe89c
Mauro Daniel viveros <maurod.viveros@gmail.com>
2026-03-12 22:55:32 -0300

    feat: :sparkles: update GET route to integrate Supabase for medal retrieval and redirect logic

--- src/pages/m/[code].ts
+++ src/pages/m/[code].ts
@@ -1,15 +1,10 @@
+import { supabase } from '@/supabase';
 import type { APIRoute } from 'astro';
 
-export const GET: APIRoute = async ({ params }) => {
+export const GET: APIRoute = async ({ params, redirect }) => {
   const { code } = params;
-
-  // Add your redirect logic here
-  const targetUrl = `https://example.com/${code}`;
-
-  return new Response(null, {
-    status: 301,
-    headers: {
-      'Location': targetUrl,
-    },
-  });
+  if(!code) return redirect('/404');
+  const { data } = await supabase.from('medals').select('id').eq('code', code).single();
+  if(!data) return redirect('/404');
+  return redirect(`/medal/${data.id}`, 307)
 };
