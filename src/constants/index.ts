64555ef96a0c915b6088d670cac2a011649f6311
Mauro Daniel viveros <maurod.viveros@gmail.com>
2026-03-20 20:24:20 -0300

    feat: ✨ add PURCHASE_LINK constant and update button components for improved purchase flow

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,5 @@
 export * from './labels';
 export * from './landing';
 export * from './resend';
+
+export const PURCHASE_LINK: string | null = "https://articulo.mercadolibre.com.ar/MLA-3024740610-chapa-identificacion-qr-kodal-_JM";
