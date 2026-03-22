6bb3c99961617190b7e0098f669213fe0848552f
Mauro Daniel viveros <maurod.viveros@gmail.com>
2026-03-21 21:41:29 -0300

    revert: :rewind: add font configuration for 'Inter Variable' in astro.config and remove unused import from global.css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig, fontProviders } from 'astro/config';
+import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import vercel from '@astrojs/vercel';
 import icon from 'astro-icon';
@@ -16,13 +16,5 @@ export default defineConfig({
     plugins: [tailwindcss()]
   },
   integrations: [icon(), react()],
-  adapter: vercel(),
-  fonts: [{
-    provider: fontProviders.npm(),
-    name: 'Inter Variable',
-    cssVariable: '--font-inter',
-    options: {
-      package: '@fontsource-variable/inter',
-    }
-  }]
+  adapter: vercel()
 });
