ba68455504b09a9fda7445e81e36a5987b57ac86
Mauro Daniel viveros <maurod.viveros@gmail.com>
2026-06-01 15:34:16 -0300

    fix: :wrench: update PURCHARSE_LINK to point to the shop page

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,4 +2,4 @@ export * from './labels';
 export * from './landing';
 export * from './resend';
 
-export const PURCHASE_LINK: string | null = "https://articulo.mercadolibre.com.ar/MLA-3024740610-chapa-identificacion-qr-kodal-_JM";
+export const PURCHASE_LINK: string | null = "/shop";
